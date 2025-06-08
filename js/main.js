55fb61ae560792c3968a905e5a075f785944965c
hussain jamil <hosen3919@gmail.com>
2025-06-09 00:39:17 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let leftBtn = document.getElementById('left');
+let rightBtn = document.getElementById('right');
+
+leftBtn.onclick = () => {
+    window.location.href = 'shawarmaMenu.html';
+};
+
+rightBtn.onclick = () => {
+    window.location.href = 'piesMenu.html';
+};
